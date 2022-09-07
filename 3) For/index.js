51ba33073953cb99917dda14ca9ac1adf292a547
= <ismoilbek20050421@gmail.com>
2022-09-07 08:07:06 +0500

    JAVASCRIPT MASALALAR

---
+++ 3) For/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+
+
+/*
+1) K va n butun sonlari berilgan (n < 0).
+k sonini n marta chiqaruvchi programma tuzilsin.
+*/
+
+// let k = 1;
+// let n = 10;
+// for(let i = 0; i <= n; i++){
+//   console.log(`n: ${n}; k: ${k}`);
+// }
+
+/* 
+2) a va b butun sonlari berilgan (a < b).
+a va b sonlari orasidagi barcha butun sonlarni (a va b ni ham)
+chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi programma tuzilsin.
+(a va b ham chiqarilsin).
+*/
+
+// let a = 1;
+// let b = 10;
+// for(let i = a; i <= b; i++){
+//   console.log(i);
+// }
+
+/* 
+3) a va b butun sonlari berilgan (a < b). 
+a va b sonlari orasidagi barcha butun sonlarni (a va b dan tashqari)
+kamayish tartibida chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi
+programma tuzilsin.
+*/
+
+
