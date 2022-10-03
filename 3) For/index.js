56c8a61cc45e8bf1178e0f42262cc59bacdee423
= <ismoilbek20050421@gmail.com>
2022-10-03 23:43:46 +0500

    JAVASCRIPT MASALALAR

--- 3) For/index.js
+++ 3) For/index.js
@@ -54,5 +54,71 @@ programma tuzilsin.
 
 
 /* 
-5) 
+5) Bir kg konfetning narxi berilgan (haqiqiy son).
+0.1, 0.2, ..., 0.9, 1kg konfetning narxini chiqaruvchi 
+programma tuzilsin.
 */
+// let x = 1000;
+// for(let i = 0; i <= 1; i+=0.1){
+//   console.log(`${Math.round(i * 10) / 10} kg konfetning narxi: ${Math.round(i * 10) / 10 * x} so'm`);
+// }
+
+
+/* 
+6) Bir kg konfetning narxi berilgan (haqiqiy son).
+1.2, 1.4, ..., 2kg konfetni narxini chiqaruvchi 
+programma tuzilsin.
+*/
+
+// let sum = 1000;
+// for(let i = 1 - 0.8; i <= 2; i+=0.2){
+//   console.log(`${Math.round(i * 10) / 10} kg konfetning narxi: ${(Math.round(i * 10) / 10) * sum} so'm`);
+// }
+
+
+/* 
+7) a va b butun sonlari berilgan (a < b).
+a dan b gacha bo'lgan barcha butun sonlar yig'indisini 
+chiqaruvchi programma tuzing.
+*/
+
+// let a = 1;
+// let b = 10;
+// let x = 0;
+
+// for(let i = a; i <= b; i++){
+//   x += i;
+//   console.log(x);
+// }
+
+
+/* 
+8) a va b butun sonlari berilgan (a < b).
+a dan b gacha bo'lgan barcha butun sonlar kopaytmasini 
+chiqaruvchi programma tuzing.
+*/
+
+// let a = 1;
+// let b = 10;
+// let x = 1;
+
+// for(let i = a; i <= b; i++){
+//   x *= i;
+//   console.log(x);
+// }
+
+
+/* 
+9) a va b butun sonlari berilgan (a < b).
+a dan b gacha bo'lgan barcha butun sonlar kvadratlarning
+yigindisini chiqaruvchi programma tuzing.
+*/
+
+let a = 2;
+let b = 4;
+let x = 1;
+
+for(let i = a; i <= b; i++){
+  x *= i * i;
+  console.log(x);
+}
